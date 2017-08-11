4abb0b9147433fc058a662f60d8211c649961db4
Xiaoguo <xiaoguo.pok@goldenequator.com>
2017-08-11 18:19:31 +0800

    remove createJSModule

--- android/src/main/java/com/cboy/rn/splashscreen/SplashScreenReactPackage.java
+++ android/src/main/java/com/cboy/rn/splashscreen/SplashScreenReactPackage.java
@@ -19,7 +19,6 @@ import java.util.List;
  */
 public class SplashScreenReactPackage implements ReactPackage {
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
